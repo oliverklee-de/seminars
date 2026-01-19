04147414a6e930b59fe7f22e06622ca26b41c303
Oliver Klee <typo3-coding@oliverklee.de>
2026-01-19 18:00:10 +0100

    [TASK] Use the TYPO3 TF for `DownloadRegistrationListViewTest`

--- Tests/Functional/Csv/DownloadRegistrationListViewTest.php
+++ Tests/Functional/Csv/DownloadRegistrationListViewTest.php
@@ -6,12 +6,7 @@ namespace OliverKlee\Seminars\Tests\Functional\Csv;
 
 use OliverKlee\Oelib\Configuration\ConfigurationRegistry;
 use OliverKlee\Oelib\Configuration\DummyConfiguration;
-use OliverKlee\Oelib\Testing\TestingFramework;
 use OliverKlee\Seminars\Csv\DownloadRegistrationListView;
-use OliverKlee\Seminars\Tests\Support\LanguageHelper;
-use TYPO3\CMS\Core\Context\Context;
-use TYPO3\CMS\Core\Context\DateTimeAspect;
-use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\TestingFramework\Core\Functional\FunctionalTestCase;
 
 /**
@@ -20,8 +15,6 @@ use TYPO3\TestingFramework\Core\Functional\FunctionalTestCase;
  */
 final class DownloadRegistrationListViewTest extends FunctionalTestCase
 {
-    use LanguageHelper;
-
     protected array $testExtensionsToLoad = [
         'oliverklee/feuserextrafields',
         'oliverklee/oelib',
@@ -30,53 +23,32 @@ final class DownloadRegistrationListViewTest extends FunctionalTestCase
 
     private DownloadRegistrationListView $subject;
 
-    private TestingFramework $testingFramework;
-
     private DummyConfiguration $configuration;
-
-    /**
-     * UID of a test event record
-     */
-    private int $eventUid = 0;
 
     protected function setUp(): void
     {
         parent::setUp();
-
-        GeneralUtility::makeInstance(Context::class)
-            ->setAspect('date', new DateTimeAspect(new \DateTimeImmutable('2018-04-26 12:42:23')));
-
-        $this->getLanguageService()->includeLLFile('EXT:core/Resources/Private/Language/locallang_general.xlf');
-        $this->getLanguageService()->includeLLFile('EXT:seminars/Resources/Private/Language/locallang_db.xlf');
-
-        $this->testingFramework = new TestingFramework('tx_seminars');
 
         $configurationRegistry = ConfigurationRegistry::getInstance();
         $configurationRegistry->set('plugin', new DummyConfiguration());
         $this->configuration = new DummyConfiguration();
         $configurationRegistry->set('plugin.tx_seminars', $this->configuration);
 
-        $pageUid = $this->testingFramework->createSystemFolder();
-        $this->eventUid = $this->testingFramework->createRecord(
-            'tx_seminars_seminars',
-            [
-                'pid' => $pageUid,
-                'begin_date' => GeneralUtility::makeInstance(Context::class)->getPropertyFromAspect(
-                    'date',
-                    'timestamp',
-                ),
-            ],
-        );
-
         $this->subject = new DownloadRegistrationListView();
-        $this->subject->setEventUid($this->eventUid);
     }
 
-    protected function tearDown(): void
+    /**
+     * @test
+     */
+    public function renderForEventWithoutRegistrationsHasHeadersOnly(): void
     {
-        $this->testingFramework->cleanUpWithoutDatabase();
+        $this->importCSVDataSet(__DIR__ . '/Fixtures/DownloadRegistrationListView/EventWithoutRegistrations.csv');
+        $this->subject->setEventUid(1);
 
-        parent::tearDown();
+        $this->configuration->setAsString('fieldsFromFeUserForCsv', '');
+        $this->configuration->setAsString('fieldsFromAttendanceForCsv', 'uid');
+
+        self::assertStringContainsString('tx_seminars_attendances.uid', $this->subject->render());
     }
 
     /**
@@ -84,23 +56,13 @@ final class DownloadRegistrationListViewTest extends FunctionalTestCase
      */
     public function renderCanContainOneRegistrationUid(): void
     {
+        $this->importCSVDataSet(__DIR__ . '/Fixtures/DownloadRegistrationListView/EventWithRegistration.csv');
+        $this->subject->setEventUid(1);
+
         $this->configuration->setAsString('fieldsFromFeUserForCsv', '');
         $this->configuration->setAsString('fieldsFromAttendanceForCsv', 'uid');
 
-        $registrationUid = $this->testingFramework->createRecord(
-            'tx_seminars_attendances',
-            [
-                'seminar' => $this->eventUid,
-                'crdate' => (int)GeneralUtility::makeInstance(Context::class)
-                    ->getPropertyFromAspect('date', 'timestamp'),
-                'user' => $this->testingFramework->createFrontEndUser(),
-            ],
-        );
-
-        self::assertStringContainsString(
-            (string)$registrationUid,
-            $this->subject->render(),
-        );
+        self::assertStringContainsString('1', $this->subject->render());
     }
 
     /**
@@ -108,29 +70,12 @@ final class DownloadRegistrationListViewTest extends FunctionalTestCase
      */
     public function renderContainsFrontEndUserFieldsForDownload(): void
     {
-        $firstName = 'John';
-        $lastName = 'Doe';
+        $this->importCSVDataSet(__DIR__ . '/Fixtures/DownloadRegistrationListView/EventWithRegistration.csv');
+        $this->subject->setEventUid(1);
 
         $this->configuration->setAsString('fieldsFromFeUserForCsv', 'first_name');
-        $this->configuration->setAsString('fieldsFromFeUserForEmailCsv', 'last_name');
 
-        $this->testingFramework->createRecord(
-            'tx_seminars_attendances',
-            [
-                'seminar' => $this->eventUid,
-                'crdate' => (int)GeneralUtility::makeInstance(Context::class)
-                    ->getPropertyFromAspect('date', 'timestamp'),
-                'user' => $this->testingFramework->createFrontEndUser(
-                    '',
-                    ['first_name' => $firstName, 'last_name' => $lastName],
-                ),
-            ],
-        );
-
-        self::assertStringContainsString(
-            $firstName,
-            $this->subject->render(),
-        );
+        self::assertStringContainsString('John', $this->subject->render());
     }
 
     /**
@@ -138,29 +83,13 @@ final class DownloadRegistrationListViewTest extends FunctionalTestCase
      */
     public function renderNotContainsFrontEndUserFieldsForEmail(): void
     {
-        $firstName = 'John';
-        $lastName = 'Doe';
+        $this->importCSVDataSet(__DIR__ . '/Fixtures/DownloadRegistrationListView/EventWithRegistration.csv');
+        $this->subject->setEventUid(1);
 
         $this->configuration->setAsString('fieldsFromFeUserForCsv', 'first_name');
         $this->configuration->setAsString('fieldsFromFeUserForEmailCsv', 'last_name');
 
-        $this->testingFramework->createRecord(
-            'tx_seminars_attendances',
-            [
-                'seminar' => $this->eventUid,
-                'crdate' => (int)GeneralUtility::makeInstance(Context::class)
-                    ->getPropertyFromAspect('date', 'timestamp'),
-                'user' => $this->testingFramework->createFrontEndUser(
-                    '',
-                    ['first_name' => $firstName, 'last_name' => $lastName],
-                ),
-            ],
-        );
-
-        self::assertStringNotContainsString(
-            $lastName,
-            $this->subject->render(),
-        );
+        self::assertStringNotContainsString('Doe', $this->subject->render());
     }
 
     /**
@@ -168,28 +97,13 @@ final class DownloadRegistrationListViewTest extends FunctionalTestCase
      */
     public function renderContainsRegistrationFieldsForDownload(): void
     {
-        $knownFrom = 'Google';
-        $notes = 'Looking forward to the event!';
+        $this->importCSVDataSet(__DIR__ . '/Fixtures/DownloadRegistrationListView/EventWithRegistration.csv');
+        $this->subject->setEventUid(1);
 
         $this->configuration->setAsString('fieldsFromAttendanceForCsv', 'known_from');
         $this->configuration->setAsString('fieldsFromAttendanceForEmailCsv', 'notes');
 
-        $this->testingFramework->createRecord(
-            'tx_seminars_attendances',
-            [
-                'seminar' => $this->eventUid,
-                'crdate' => (int)GeneralUtility::makeInstance(Context::class)
-                    ->getPropertyFromAspect('date', 'timestamp'),
-                'user' => $this->testingFramework->createFrontEndUser(),
-                'known_from' => $knownFrom,
-                'notes' => $notes,
-            ],
-        );
-
-        self::assertStringContainsString(
-            $knownFrom,
-            $this->subject->render(),
-        );
+        self::assertStringContainsString('Google', $this->subject->render());
     }
 
     /**
@@ -197,52 +111,26 @@ final class DownloadRegistrationListViewTest extends FunctionalTestCase
      */
     public function renderNotContainsRegistrationFieldsForEmail(): void
     {
-        $knownFrom = 'Google';
-        $notes = 'Looking forward to the event!';
+        $this->importCSVDataSet(__DIR__ . '/Fixtures/DownloadRegistrationListView/EventWithRegistration.csv');
+        $this->subject->setEventUid(1);
 
         $this->configuration->setAsString('fieldsFromAttendanceForCsv', 'known_from');
         $this->configuration->setAsString('fieldsFromAttendanceForEmailCsv', 'notes');
 
-        $this->testingFramework->createRecord(
-            'tx_seminars_attendances',
-            [
-                'seminar' => $this->eventUid,
-                'crdate' => (int)GeneralUtility::makeInstance(Context::class)
-                    ->getPropertyFromAspect('date', 'timestamp'),
-                'user' => $this->testingFramework->createFrontEndUser(),
-                'known_from' => $knownFrom,
-                'notes' => $notes,
-            ],
-        );
-
-        self::assertStringNotContainsString(
-            $notes,
-            $this->subject->render(),
-        );
+        self::assertStringNotContainsString('Looking forward to the event!', $this->subject->render());
     }
 
     /**
      * @test
      */
-    public function renderForQueueRegistrationsDoesNotContainRegistrationOnQueue(): void
+    public function renderDoesNotContainRegistrationOnQueue(): void
     {
+        $this->importCSVDataSet(__DIR__ . '/Fixtures/DownloadRegistrationListView/EventWithQueueRegistration.csv');
+        $this->subject->setEventUid(1);
+
         $this->configuration->setAsString('fieldsFromAttendanceForCsv', 'uid');
         $this->configuration->setAsString('fieldsFromAttendanceForEmailCsv', 'uid');
 
-        $registrationUid = $this->testingFramework->createRecord(
-            'tx_seminars_attendances',
-            [
-                'seminar' => $this->eventUid,
-                'crdate' => (int)GeneralUtility::makeInstance(Context::class)
-                    ->getPropertyFromAspect('date', 'timestamp'),
-                'user' => $this->testingFramework->createFrontEndUser(),
-                'registration_queue' => true,
-            ],
-        );
-
-        self::assertStringNotContainsString(
-            (string)$registrationUid,
-            $this->subject->render(),
-        );
+        self::assertStringNotContainsString('1', $this->subject->render());
     }
 }
