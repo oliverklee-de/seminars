8c2cfaa45551d5f672dd865e3ce876f5f03ffdde
Oliver Klee <typo3-coding@oliverklee.de>
2026-02-27 20:33:31 +0100

    [TASK] Avoid unnecessary mocking in a test
    Mocking `ContentObjectRenderer` poses some problems, and using
    the real thing does so as well:
    
    - A mock does not implement `__sleep()`, which triggers some
      warnings.
    
    - The real thing depends on the PSR-11 container present, which
      is not the case in the unit tests.

---
+++ Tests/Unit/Templating/Fixtures/TestingContentObjectRenderer.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+namespace OliverKlee\Seminars\Tests\Unit\Templating\Fixtures;
+
+use TYPO3\CMS\Frontend\ContentObject\ContentObjectRenderer;
+
+final class TestingContentObjectRenderer extends ContentObjectRenderer
+{
+    public function __construct()
+    {
+    }
+}
