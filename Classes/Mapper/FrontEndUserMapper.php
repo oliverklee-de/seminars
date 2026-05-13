0900dcd1673aaf0d1a0c7d5eba1a3ec30a9b1a27
Oliver Klee <typo3-coding@oliverklee.de>
2026-05-13 14:20:22 +0200

    [TASK] Drop the Model FE user -> group association
    This association is not used in the seminars extension.
    
    Fixes #3687

--- Classes/Mapper/FrontEndUserMapper.php
+++ Classes/Mapper/FrontEndUserMapper.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace OliverKlee\Seminars\Mapper;
 
 use OliverKlee\Oelib\Mapper\AbstractDataMapper;
-use OliverKlee\Oelib\Mapper\FrontEndUserGroupMapper;
 use OliverKlee\Seminars\Model\FrontEndUser;
 
 /**
@@ -20,7 +19,6 @@ class FrontEndUserMapper extends AbstractDataMapper
     protected $modelClassName = FrontEndUser::class;
 
     protected $relations = [
-        'usergroup' => FrontEndUserGroupMapper::class,
         'tx_seminars_registration' => RegistrationMapper::class,
     ];
 }
