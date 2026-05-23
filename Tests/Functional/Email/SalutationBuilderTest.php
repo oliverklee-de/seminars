4c166f406564c35a98b1ce8c1fb8633ee15a0418
Oliver Klee <typo3-coding@oliverklee.de>
2026-05-23 09:55:49 +0200

    [TASK] Rework `SalutationBuilderTest`

--- Tests/Functional/Email/SalutationBuilderTest.php
+++ Tests/Functional/Email/SalutationBuilderTest.php
@@ -4,13 +4,13 @@ declare(strict_types=1);
 
 namespace OliverKlee\Seminars\Tests\Functional\Email;
 
-use OliverKlee\Oelib\Mapper\MapperRegistry;
-use OliverKlee\Oelib\Testing\TestingFramework;
+use OliverKlee\Oelib\Configuration\ConfigurationRegistry;
+use OliverKlee\Oelib\Configuration\DummyConfiguration;
 use OliverKlee\Seminars\Email\SalutationBuilder;
-use OliverKlee\Seminars\Mapper\FrontEndUserMapper;
 use OliverKlee\Seminars\Model\FrontEndUser;
-use OliverKlee\Seminars\Tests\Support\BackEndTestsTrait;
 use OliverKlee\Seminars\Tests\Unit\OldModel\Fixtures\TestingLegacyEvent;
+use TYPO3\CMS\Core\Localization\LanguageServiceFactory;
+use TYPO3\CMS\Extbase\Utility\LocalizationUtility;
 use TYPO3\TestingFramework\Core\Functional\FunctionalTestCase;
 
 /**
@@ -18,11 +18,9 @@ use TYPO3\TestingFramework\Core\Functional\FunctionalTestCase;
  */
 final class SalutationBuilderTest extends FunctionalTestCase
 {
-    use BackEndTestsTrait;
-
     private const DATE_FORMAT = 'Y-m-d';
     private const TIME_FORMAT = 'H:i';
-    private const SECONDS_PER_DAY = 86400;
+    private const SECONDS_PER_HOUR = 3600;
 
     protected array $testExtensionsToLoad = [
         'oliverklee/feuserextrafields',
@@ -30,9 +28,7 @@ final class SalutationBuilderTest extends FunctionalTestCase
         'oliverklee/seminars',
     ];
 
-    private TestingFramework $testingFramework;
-
-    private FrontEndUserMapper $frontEndUserMapper;
+    private DummyConfiguration $configuration;
 
     private SalutationBuilder $subject;
 
@@ -40,42 +36,23 @@ final class SalutationBuilderTest extends FunctionalTestCase
     {
         parent::setUp();
 
-        $this->unifyTestingEnvironment();
+        $this->configuration = new DummyConfiguration();
+        $this->get(ConfigurationRegistry::class)->set('plugin.tx_seminars', $this->configuration);
 
-        $this->testingFramework = $this->get(TestingFramework::class);
-        $this->configuration->setAsString('salutation', 'formal');
-        $this->frontEndUserMapper = $this->get(MapperRegistry::class)->getByClassName(FrontEndUserMapper::class);
+        $this->importCSVDataSet(__DIR__ . '/Fixtures/SalutationBuilder/AdminBackendUser.csv');
+        $GLOBALS['LANG'] = $this
+            ->get(LanguageServiceFactory::class)
+            ->createFromUserPreferences($this->setUpBackendUser(1));
 
         $this->subject = $this->get(SalutationBuilder::class);
     }
 
-    protected function tearDown(): void
+    private static function assertNotContainsRawLabelKey(string $string): void
     {
-        $this->restoreOriginalEnvironment();
-
-        $this->testingFramework->cleanUpWithoutDatabase();
-
-        parent::tearDown();
-    }
-
-    // Utility functions
-
-    /**
-     * Creates an FE-user with the name "Foo".
-     */
-    private function createFrontEndUser(): FrontEndUser
-    {
-        return $this->frontEndUserMapper->getLoadedTestingModel(['name' => 'Foo']);
-    }
-
-    // Tests concerning the utility functions
-
-    /**
-     * @test
-     */
-    public function createFrontEndUserReturnsFeUserModel(): void
-    {
-        self::assertInstanceOf(FrontEndUser::class, $this->createFrontEndUser());
+        self::assertStringNotContainsString('_', $string);
+        self::assertStringNotContainsString('email', $string);
+        self::assertStringNotContainsString('formal', $string);
+        self::assertStringNotContainsString('salutation', $string);
     }
 
     /**
@@ -86,121 +63,112 @@ final class SalutationBuilderTest extends FunctionalTestCase
         self::assertInstanceOf(SalutationBuilder::class, $this->get(SalutationBuilder::class));
     }
 
-    // Tests concerning getSalutation
+    /**
+     * @return array<non-empty-string, array{0: string}>
+     */
+    public static function salutationModeDataProvider(): array
+    {
+        return [
+            'formal' => ['formal'],
+            'informal' => ['informal'],
+            'empty' => [''],
+        ];
+    }
 
     /**
      * @test
+     * @dataProvider salutationModeDataProvider
      */
-    public function getSalutationReturnsUsernameOfRegistration(): void
+    public function getSalutationForAllSalutationModesReturnsFullNameOfRegisteredUser(string $salutationMode): void
     {
-        self::assertStringContainsString(
-            'Foo',
-            $this->subject->getSalutation($this->createFrontEndUser()),
-        );
+        $this->configuration->setAsString('salutation', $salutationMode);
+
+        $fullName = 'Max Minimax';
+        $user = new FrontEndUser();
+        $user->setName($fullName);
+
+        $result = $this->subject->getSalutation($user);
+
+        self::assertStringContainsString($fullName, $result);
     }
 
     /**
      * @test
      */
-    public function getSalutationReturnsGenderUnspecificSalutation(): void
+    public function getSalutationReturnsGenderNeutralSalutation(): void
     {
-        $user = $this->createFrontEndUser();
+        $user = new FrontEndUser();
+        $user->setName('Max Minimax');
 
-        self::assertStringContainsString(
-            $this->translate('email_hello_formal_99'),
-            $this->subject->getSalutation($user),
-        );
+        $result = $this->subject->getSalutation($user);
+
+        $expected = LocalizationUtility::translate('email_hello_formal_99', 'seminars');
+        self::assertIsString($expected);
+        self::assertStringContainsString($expected, $result);
     }
 
     /**
      * @test
      */
-    public function getSalutationForInformalSalutationReturnsInformalSalutation(): void
+    public function getSalutationForInformalSalutationModeReturnsInformalSalutation(): void
     {
-        $user = $this->createFrontEndUser();
         $this->configuration->setAsString('salutation', 'informal');
+        $user = new FrontEndUser();
+        $user->setName('Max Minimax');
 
-        self::assertStringContainsString(
-            $this->translate('email_hello_informal'),
-            $this->subject->getSalutation($user),
-        );
+        $result = $this->subject->getSalutation($user);
+
+        $expected = LocalizationUtility::translate('email_hello_informal', 'seminars');
+        self::assertIsString($expected);
+        self::assertStringContainsString($expected, $result);
     }
 
     /**
      * @test
      */
-    public function getSalutationForInformalSalutationReturnsUsersName(): void
-    {
-        $user = $this->createFrontEndUser();
-        $this->configuration->setAsString('salutation', 'informal');
-
-        self::assertStringContainsString('Foo', $this->subject->getSalutation($user));
-    }
-
-    /**
-     * @test
-     */
-    public function getSalutationForFormalSalutationModeContainsNoRawLabelKeys(): void
+    public function getSalutationForFormalSalutationModeReturnsFormalSalutation(): void
     {
         $this->configuration->setAsString('salutation', 'formal');
+        $user = new FrontEndUser();
+        $user->setName('Max Minimax');
 
-        $user = $this->createFrontEndUser();
-        $salutation = $this->subject->getSalutation($user);
+        $result = $this->subject->getSalutation($user);
 
-        self::assertStringNotContainsString(
-            '_',
-            $salutation,
-        );
-        self::assertStringNotContainsString(
-            'salutation',
-            $salutation,
-        );
-        self::assertStringNotContainsString(
-            'email',
-            $salutation,
-        );
-        self::assertStringNotContainsString(
-            'formal',
-            $salutation,
-        );
+        $expected = LocalizationUtility::translate('email_hello_formal_99', 'seminars');
+        self::assertIsString($expected);
+        self::assertStringContainsString($expected, $result);
     }
 
     /**
      * @test
      */
-    public function getSalutationForInformalSalutationModeContainsNoRawLabelKeys(): void
-    {
-        $this->configuration->setAsString('salutation', 'informal');
-
-        $user = $this->createFrontEndUser();
-        $salutation = $this->subject->getSalutation($user);
-
-        $this->assertNotContainsRawLabelKey($salutation);
-    }
-
-    /**
-     * @test
-     */
-    public function getSalutationForNoSalutationModeContainsNoRawLabelKeys(): void
+    public function getSalutationForEmptySalutationModeReturnsFormalSalutation(): void
     {
         $this->configuration->setAsString('salutation', '');
+        $user = new FrontEndUser();
+        $user->setName('Max Minimax');
 
-        $user = $this->createFrontEndUser();
-        $salutation = $this->subject->getSalutation($user);
+        $result = $this->subject->getSalutation($user);
 
-        $this->assertNotContainsRawLabelKey($salutation);
+        $expected = LocalizationUtility::translate('email_hello_formal_99', 'seminars');
+        self::assertIsString($expected);
+        self::assertStringContainsString($expected, $result);
     }
 
     /**
-     * Checks that $string does not contain a raw label key.
-     *
-     * @param string $string
+     * @test
+     * @dataProvider salutationModeDataProvider
      */
-    private function assertNotContainsRawLabelKey(string $string): void
+    public function getSalutationForAllSalutationModesContainsNoRawLabelKeys(string $salutationMode): void
     {
-        self::assertStringNotContainsString('_', $string);
-        self::assertStringNotContainsString('salutation', $string);
-        self::assertStringNotContainsString('formal', $string);
+        $this->configuration->setAsString('salutation', $salutationMode);
+
+        $user = new FrontEndUser();
+        $user->setName('Max Minimax');
+
+        $result = $this->subject->getSalutation($user);
+
+        self::assertNotContainsRawLabelKey($result);
     }
 
     // Tests concerning createIntroduction
@@ -211,10 +179,10 @@ final class SalutationBuilderTest extends FunctionalTestCase
     public function createIntroductionWithEmptyBeginThrowsException(): void
     {
         $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('$introductionBegin must not be empty.');
+        $this->expectExceptionCode(1440109640);
 
-        $eventUid = $this->testingFramework->createRecord('tx_seminars_seminars');
-
-        $event = new TestingLegacyEvent($eventUid);
+        $event = TestingLegacyEvent::fromData(['begin_date' => 0]);
 
         $this->subject->createIntroduction('', $event);
     }
@@ -224,17 +192,13 @@ final class SalutationBuilderTest extends FunctionalTestCase
      */
     public function createIntroductionForEventWithDateReturnsEventsDate(): void
     {
-        $eventUid = $this->testingFramework->createRecord(
-            'tx_seminars_seminars',
-            ['begin_date' => $this->now],
-        );
+        $beginDate = 1779529905;
+        $event = TestingLegacyEvent::fromData(['begin_date' => $beginDate]);
 
-        $event = new TestingLegacyEvent($eventUid);
+        $result = $this->subject->createIntroduction('%s', $event);
 
-        self::assertStringContainsString(
-            \date(self::DATE_FORMAT, $this->now),
-            $this->subject->createIntroduction('%s', $event),
-        );
+        $expected = \date(self::DATE_FORMAT, $beginDate);
+        self::assertStringContainsString($expected, $result);
     }
 
     /**
@@ -242,19 +206,14 @@ final class SalutationBuilderTest extends FunctionalTestCase
      */
     public function createIntroductionForEventWithBeginAndEndDateOnDifferentDaysReturnsEventsDateFromTo(): void
     {
-        $eventUid = $this->testingFramework->createRecord(
-            'tx_seminars_seminars',
-            [
-                'begin_date' => $this->now,
-                'end_date' => $this->now + self::SECONDS_PER_DAY,
-            ],
-        );
-        $event = new TestingLegacyEvent($eventUid);
+        $beginDate = 1779529905;
+        $endDate = 1779929905;
+        $event = TestingLegacyEvent::fromData(['begin_date' => $beginDate, 'end_date' => $endDate]);
 
-        self::assertStringContainsString(
-            \date(self::DATE_FORMAT, $this->now) . '-' . \date(self::DATE_FORMAT, $this->now + self::SECONDS_PER_DAY),
-            $this->subject->createIntroduction('%s', $event),
-        );
+        $result = $this->subject->createIntroduction('%s', $event);
+
+        $expected = \date(self::DATE_FORMAT, $beginDate) . '-' . \date(self::DATE_FORMAT, $endDate);
+        self::assertStringContainsString($expected, $result);
     }
 
     /**
@@ -262,19 +221,13 @@ final class SalutationBuilderTest extends FunctionalTestCase
      */
     public function createIntroductionForEventWithTimeReturnsEventsTime(): void
     {
-        $eventUid = $this->testingFramework->createRecord(
-            'tx_seminars_seminars',
-            [
-                'begin_date' => $this->now,
-            ],
-        );
+        $beginDate = 1779529905;
+        $event = TestingLegacyEvent::fromData(['begin_date' => $beginDate]);
 
-        $event = new TestingLegacyEvent($eventUid);
+        $result = $this->subject->createIntroduction('%s', $event);
 
-        self::assertStringContainsString(
-            \date(self::TIME_FORMAT, $this->now),
-            $this->subject->createIntroduction('%s', $event),
-        );
+        $expected = \date(self::TIME_FORMAT, $beginDate);
+        self::assertStringContainsString($expected, $result);
     }
 
     /**
@@ -282,24 +235,21 @@ final class SalutationBuilderTest extends FunctionalTestCase
      */
     public function createIntroductionForEventWithStartAndEndOnOneDayReturnsTimeFromTo(): void
     {
-        $endDate = $this->now + 3600;
-        $eventUid = $this->testingFramework->createRecord(
-            'tx_seminars_seminars',
-            [
-                'begin_date' => $this->now,
-                'end_date' => $endDate,
-            ],
-        );
+        $beginDate = 1779529905;
+        $endDate = 1779529905 + self::SECONDS_PER_HOUR;
+        $event = TestingLegacyEvent::fromData(['begin_date' => $beginDate, 'end_date' => $endDate]);
 
-        $event = new TestingLegacyEvent($eventUid);
-        $timeInsert = \date(self::TIME_FORMAT, $this->now) . ' ' .
-            $this->translate('email_timeTo') . ' ' .
-            \date(self::TIME_FORMAT, $endDate);
+        $result = $this->subject->createIntroduction('%s', $event);
 
-        self::assertStringContainsString(
-            \sprintf($this->translate('email_timeFrom'), $timeInsert),
-            $this->subject->createIntroduction('%s', $event),
-        );
+        $timeToWithPlaceholders = LocalizationUtility::translate('email_timeTo', 'seminars');
+        self::assertIsString($timeToWithPlaceholders);
+        $timeInsert = \date(self::TIME_FORMAT, $beginDate) . ' ' . $timeToWithPlaceholders . ' '
+            . \date(self::TIME_FORMAT, $endDate);
+        $timeFromWithPlaceholders = LocalizationUtility::translate('email_timeFrom', 'seminars');
+        self::assertIsString($timeFromWithPlaceholders);
+        $expected = \sprintf($timeFromWithPlaceholders, $timeInsert);
+
+        self::assertStringContainsString($expected, $result);
     }
 
     /**
@@ -307,81 +257,28 @@ final class SalutationBuilderTest extends FunctionalTestCase
      */
     public function createIntroductionForEventWithStartAndEndOnOneDayContainsDate(): void
     {
-        $endDate = $this->now + 3600;
-        $eventUid = $this->testingFramework->createRecord(
-            'tx_seminars_seminars',
-            [
-                'begin_date' => $this->now,
-                'end_date' => $endDate,
-            ],
-        );
+        $beginDate = 1779529905;
+        $endDate = 1779529905 + self::SECONDS_PER_HOUR;
+        $event = TestingLegacyEvent::fromData(['begin_date' => $beginDate, 'end_date' => $endDate]);
 
-        $event = new TestingLegacyEvent($eventUid);
-        $formattedDate = \date(self::DATE_FORMAT, $this->now);
+        $result = $this->subject->createIntroduction('%s', $event);
 
-        self::assertStringContainsString(
-            $formattedDate,
-            $this->subject->createIntroduction('%s', $event),
-        );
+        $formattedDate = \date(self::DATE_FORMAT, $beginDate);
+        self::assertStringContainsString($formattedDate, $result);
     }
 
     /**
      * @test
+     * @dataProvider salutationModeDataProvider
      */
-    public function createIntroductionForFormalSalutationModeContainsNoRawLabelKeys(): void
+    public function createIntroductionForAllSalutationModesContainsNoRawLabelKeys(string $salutationMode): void
     {
-        $salutation = 'formal';
-        $this->configuration->setAsString('salutation', $salutation);
+        $this->configuration->setAsString('salutation', $salutationMode);
 
-        $eventUid = $this->testingFramework->createRecord(
-            'tx_seminars_seminars',
-            ['begin_date' => $this->now],
-        );
+        $event = TestingLegacyEvent::fromData(['begin_date' => 1779529905]);
 
-        $event = new TestingLegacyEvent($eventUid);
+        $result = $this->subject->createIntroduction('%s', $event);
 
-        $introduction = $this->subject->createIntroduction('%s', $event);
-
-        $this->assertNotContainsRawLabelKey($introduction);
-    }
-
-    /**
-     * @test
-     */
-    public function createIntroductionForInformalSalutationModeContainsNoRawLabelKeys(): void
-    {
-        $salutation = 'informal';
-        $this->configuration->setAsString('salutation', $salutation);
-
-        $eventUid = $this->testingFramework->createRecord(
-            'tx_seminars_seminars',
-            ['begin_date' => $this->now],
-        );
-
-        $event = new TestingLegacyEvent($eventUid);
-
-        $introduction = $this->subject->createIntroduction('%s', $event);
-
-        $this->assertNotContainsRawLabelKey($introduction);
-    }
-
-    /**
-     * @test
-     */
-    public function createIntroductionForNoSalutationModeContainsNoRawLabelKeys(): void
-    {
-        $salutation = '';
-        $this->configuration->setAsString('salutation', $salutation);
-
-        $eventUid = $this->testingFramework->createRecord(
-            'tx_seminars_seminars',
-            ['begin_date' => $this->now],
-        );
-
-        $event = new TestingLegacyEvent($eventUid);
-
-        $introduction = $this->subject->createIntroduction('%s', $event);
-
-        $this->assertNotContainsRawLabelKey($introduction);
+        self::assertNotContainsRawLabelKey($result);
     }
 }
