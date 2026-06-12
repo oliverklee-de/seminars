94f9b6f6dd29677f9b4801841c1aadc257f79054
Anna Reiter <anna.reiter@oliverklee.de>
2026-06-12 16:48:15 +0200

    [TASK] Add `description` data to `myRegistrations` single view
    Part of #4977

--- Tests/Functional/Controller/MyRegistrationsControllerTest.php
+++ Tests/Functional/Controller/MyRegistrationsControllerTest.php
@@ -829,7 +829,7 @@ final class MyRegistrationsControllerTest extends FunctionalTestCase
 
         $html = (string)$this->executeFrontendSubRequest($request, $requestContext)->getBody();
 
-        self::assertStringContainsString('<p>Großartig, großartig, sehr großartig</p>', $html);
+        self::assertStringContainsString('<p>Großartig, großartig, <em>sehr</em> großartig</p>', $html);
     }
 
     /**
