a20754d006e354cd9de0c407e403b65f2b2aa4a0
Anna Reiter <anna.reiter@oliverklee.de>
2026-07-14 23:00:00 +0200

    [TASK] Test categories display of event dates in MyRegistrations (#5270)
    Part of #5225

--- Tests/Functional/Controller/MyRegistrationsControllerTest.php
+++ Tests/Functional/Controller/MyRegistrationsControllerTest.php
@@ -392,6 +392,22 @@ final class MyRegistrationsControllerTest extends FunctionalTestCase
     /**
      * @test
      */
+    public function indexActionForEventDateRendersCategoriesOfRegistrationOfTheLoggedInUser(): void
+    {
+        $this->importCSVDataSet(self::FIXTURES_PATH . '/FrontEndUserAndGroup.csv');
+        $this->importCSVDataSet(self::FIXTURES_PATH . '/indexAction/RegistrationForEventDateWithTwoCategories.csv');
+
+        $request = (new InternalRequest())->withPageId(7);
+        $requestContext = (new InternalRequestContext())->withFrontendUserId(1);
+        $html = (string)$this->executeFrontendSubRequest($request, $requestContext)->getBody();
+
+        self::assertStringContainsString('intense', $html);
+        self::assertStringContainsString('laid-back', $html);
+    }
+
+    /**
+     * @test
+     */
     public function indexActionRendersSpeakersOfRegistrationOfTheLoggedInUser(): void
     {
         $this->importCSVDataSet(self::FIXTURES_PATH . '/FrontEndUserAndGroup.csv');
