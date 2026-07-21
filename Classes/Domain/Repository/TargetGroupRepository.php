b7bcb9767cc4e28fa39539c60bf47854490cae5f
Anna Reiter <anna.reiter@oliverklee.de>
2026-07-21 16:02:53 +0200

    [FEATURE] Add `TargetGroup` model and repository (#5277)

---
+++ Classes/Domain/Repository/TargetGroupRepository.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OliverKlee\Seminars\Domain\Repository;
+
+use OliverKlee\Seminars\Domain\Model\TargetGroup;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Extbase\Persistence\Generic\Typo3QuerySettings;
+use TYPO3\CMS\Extbase\Persistence\QueryInterface;
+use TYPO3\CMS\Extbase\Persistence\Repository;
+
+/**
+ * @extends Repository<TargetGroup>
+ */
+class TargetGroupRepository extends Repository
+{
+    protected $defaultOrderings = ['title' => QueryInterface::ORDER_ASCENDING];
+
+    public function initializeObject(): void
+    {
+        $querySettings = GeneralUtility::makeInstance(Typo3QuerySettings::class);
+        $querySettings->setRespectStoragePage(false);
+        $this->setDefaultQuerySettings($querySettings);
+    }
+}
