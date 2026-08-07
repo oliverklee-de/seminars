671a95089afdef95c64abd8ddb32ae8ffd8ab30d
Oliver Klee <typo3-coding@oliverklee.de>
2026-08-07 12:39:31 +0200

    [BUGFIX] Only load the legacy BE module registration in 11LTS (#5327)
    Fixes #5324

--- ext_tables.php
+++ ext_tables.php
@@ -4,31 +4,31 @@ use OliverKlee\Seminars\Controller\BackEnd\EmailController;
 use OliverKlee\Seminars\Controller\BackEnd\EventController;
 use OliverKlee\Seminars\Controller\BackEnd\ModuleController;
 use OliverKlee\Seminars\Controller\BackEnd\RegistrationController;
+use TYPO3\CMS\Core\Information\Typo3Version;
 use TYPO3\CMS\Extbase\Utility\ExtensionUtility;
 
 defined('TYPO3') or die('Access denied.');
 
 $boot = static function (): void {
-    /**
-     * BE module
-     */
-    ExtensionUtility::registerModule(
-        'Seminars',
-        'web',
-        'events',
-        '',
-        [
-            ModuleController::class => 'overview',
-            EventController::class => 'hide, unhide, delete, search, duplicate',
-            RegistrationController::class => 'showForEvent, exportCsvForEvent, exportCsvForPageUid, delete',
-            EmailController::class => 'compose, send',
-        ],
-        [
-            'access' => 'user,group',
-            'icon' => 'EXT:seminars/Resources/Public/Icons/BackEndModule.svg',
-            'labels' => 'LLL:EXT:seminars/Resources/Private/Language/locallang.xlf',
-        ],
-    );
+    if ((new Typo3Version())->getMajorVersion() < 12) {
+        ExtensionUtility::registerModule(
+            'Seminars',
+            'web',
+            'events',
+            '',
+            [
+                ModuleController::class => 'overview',
+                EventController::class => 'hide, unhide, delete, search, duplicate',
+                RegistrationController::class => 'showForEvent, exportCsvForEvent, exportCsvForPageUid, delete',
+                EmailController::class => 'compose, send',
+            ],
+            [
+                'access' => 'user,group',
+                'icon' => 'EXT:seminars/Resources/Public/Icons/BackEndModule.svg',
+                'labels' => 'LLL:EXT:seminars/Resources/Private/Language/locallang.xlf',
+            ],
+        );
+    }
 };
 
 $boot();
