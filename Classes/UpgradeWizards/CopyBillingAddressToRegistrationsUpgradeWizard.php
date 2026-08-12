4888fddf0211d62a2f90e1017087161f160ef821
Oliver Klee <typo3-coding@oliverklee.de>
2026-08-12 18:10:51 +0200

    [TASK] Use PHP attributes for upgrade wizards in TYPO3 >= 12LTS
    Fixes #5325

--- Classes/UpgradeWizards/CopyBillingAddressToRegistrationsUpgradeWizard.php
+++ Classes/UpgradeWizards/CopyBillingAddressToRegistrationsUpgradeWizard.php
@@ -9,6 +9,7 @@ use Psr\Log\LoggerAwareTrait;
 use TYPO3\CMS\Core\Database\Connection;
 use TYPO3\CMS\Core\Database\ConnectionPool;
 use TYPO3\CMS\Core\Database\Query\QueryBuilder;
+use TYPO3\CMS\Install\Attribute\UpgradeWizard;
 use TYPO3\CMS\Install\Updates\DatabaseUpdatedPrerequisite;
 use TYPO3\CMS\Install\Updates\RepeatableInterface;
 use TYPO3\CMS\Install\Updates\UpgradeWizardInterface;
@@ -21,6 +22,7 @@ use TYPO3\CMS\Install\Updates\UpgradeWizardInterface;
  *
  * @internal
  */
+#[UpgradeWizard('seminars_copyBillingAddressToRegistrations')]
 class CopyBillingAddressToRegistrationsUpgradeWizard implements
     UpgradeWizardInterface,
     RepeatableInterface,
