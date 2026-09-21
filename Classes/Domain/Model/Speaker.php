52cadac9262904a32afcc03ddbe0e3ca8965dac4
Anna Reiter <anna.reiter@oliverklee.de>
2026-09-21 14:51:38 +0200

    [TASK] Add getter and setter for image in `Speakers` (#5389)

--- Classes/Domain/Model/Speaker.php
+++ Classes/Domain/Model/Speaker.php
@@ -5,8 +5,11 @@ declare(strict_types=1);
 namespace OliverKlee\Seminars\Domain\Model;
 
 use OliverKlee\Oelib\Interfaces\MailRole;
+use TYPO3\CMS\Extbase\Annotation\ORM\Lazy;
 use TYPO3\CMS\Extbase\Annotation\Validate;
+use TYPO3\CMS\Extbase\Domain\Model\FileReference;
 use TYPO3\CMS\Extbase\DomainObject\AbstractEntity;
+use TYPO3\CMS\Extbase\Persistence\Generic\LazyLoadingProxy;
 
 /**
  * This class represents a speaker for an event.
@@ -32,6 +35,13 @@ class Speaker extends AbstractEntity implements MailRole
      * @Validate("StringLength", options={"maximum": 255})
      */
     protected string $homepage = '';
+
+    /**
+     * @var FileReference|null
+     * @phpstan-var FileReference|LazyLoadingProxy|null
+     * @Lazy
+     */
+    protected $image;
 
     public function getName(): string
     {
@@ -71,5 +81,22 @@ class Speaker extends AbstractEntity implements MailRole
     public function setHomepage(string $homepage): void
     {
         $this->homepage = $homepage;
+    }
+
+    public function getImage(): ?FileReference
+    {
+        $image = $this->image;
+        if ($image instanceof LazyLoadingProxy) {
+            $image = $image->_loadRealInstance();
+            $image = ($image instanceof FileReference) ? $image : null;
+            $this->image = $image;
+        }
+
+        return $image;
+    }
+
+    public function setImage(?FileReference $image): void
+    {
+        $this->image = $image;
     }
 }
