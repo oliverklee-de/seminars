a06bac3c05b9c9e4276e8f308b54f0b4ecbd8536
Oliver Klee <typo3-coding@oliverklee.de>
2026-09-21 19:28:01 +0200

    [CLEANUP] Drop some overtight assertions

--- Classes/Email/SalutationBuilder.php
+++ Classes/Email/SalutationBuilder.php
@@ -75,20 +75,14 @@ class SalutationBuilder
 
         $eventDateLabel = LocalizationUtility::translate('email_eventDate', 'seminars');
         \assert(\is_string($eventDateLabel));
-        \assert($eventDateLabel !== '');
         $result .= ' ' . \sprintf($eventDateLabel, $event->getDate('-'));
 
         if ($event->hasTime() && !$event->hasTimeslots()) {
             $timeToLabelWithPlaceholders = LocalizationUtility::translate('email_timeTo', 'seminars');
-            \assert(\is_string($timeToLabelWithPlaceholders));
-            \assert($timeToLabelWithPlaceholders !== '');
-            $time = $event->getTime(' ' . $timeToLabelWithPlaceholders . ' ');
             $timeFromLabel = LocalizationUtility::translate('email_timeFrom', 'seminars');
-            \assert(\is_string($timeFromLabel));
-            \assert($timeFromLabel !== '');
             $timeAtLabel = LocalizationUtility::translate('email_timeAt', 'seminars');
-            \assert(\is_string($timeAtLabel));
-            \assert($timeAtLabel !== '');
+            \assert(\is_string($timeToLabelWithPlaceholders) && \is_string($timeFromLabel) && \is_string($timeAtLabel));
+            $time = $event->getTime(' ' . $timeToLabelWithPlaceholders . ' ');
             $label = ' ' . (!$event->isOpenEnded() ? $timeFromLabel : $timeAtLabel);
             $result .= \sprintf($label, $time);
         }
